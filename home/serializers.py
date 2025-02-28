8c0f5d9252ee6ae5db265b449f3f4be0ba885a3e
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2025-02-28 12:49:48 +0530

    Creating Serializers

---
+++ home/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers  
+from models import *
+
+class FileListSerializer(serializers.Serializer): 
+    files = serializers.ListField( 
+        child = serializers.FileField(max_length=100000, allow_empty_file = False, use_url= False)) 
+    
+    def create(self, validated_data): 
+        folder = Folder.objects.create() 
+        files = validated_data.pop('files')  
+        files_obj = [] 
+
+        for file in files: 
+            files_obj = Files.objects.create(folder = folder, file = file) 
+            files_obj.append(files_obj) 
+
+        return files_obj
